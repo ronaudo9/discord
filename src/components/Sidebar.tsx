9444d0110eff94311325d93bca7b2b5e4fc96a89
ronaudo9 <naoyuki.kojima@rakus-partners.co.jp>
2023-03-14 09:27:33 +0900

    "first commit”

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import "./Sidebar.scss";
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+
+const Sidebar = () => {
+  return (
+    <div className="sidebar">
+      {/*sidebarLeft*/}
+      <div className="sidebarLeft">
+        <div className="serverIcon">
+          <img src="./logo192.png" alt="" />
+        </div>
+        <div className="serverIcon">
+          <img src="./logo192.png" alt="" />
+        </div>
+      </div>
+      {/*sidebarRight*/}
+      <div className="sidebarRight">
+        <div className="sidebarTop">
+          <h3>Discord</h3>
+          <ExpandMoreIcon/>
+        </div>
+         {/*sidebarChannels*/}
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
